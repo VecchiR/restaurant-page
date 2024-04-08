57fb2e311a001d4ce7e474144e893217f73496cc
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-04-08 15:51:44 -0300

    Create event listeners for nav buttons

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import { loadHome } from "./home";
 
 console.log("heyoo - I'm coming from src/index.js");
 loadHome();
+
+
+const tabs = document.querySelectorAll('.tab-btn');
+tabs.forEach((btn) => btn.addEventListener('click', () => {
+    if (btn.classList.contains('home')) {
+        console.log('this is home');
+    }
+    else if (btn.classList.contains('menu')) {
+        console.log('this is menu');
+    }
+    else if (btn.classList.contains('about')) {
+        console.log('this is about');
+    }
+}));
