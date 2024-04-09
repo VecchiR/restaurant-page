2581bf8f50269d1c986ac7df8bf92b404b52746e
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-04-09 17:25:38 -0300

    Add logo to header and keep image for all pages

--- src/home.js
+++ src/home.js
@@ -9,15 +9,11 @@ headline.setAttribute('class', 'headline');
 const line1 = document.createElement('span');
 line1.innerHTML = 'ditch the dull,';
 const line2 = document.createElement('span');
-line2.innerHTML = 'chill with the cool.';
-const icy = new Image();
-icy.src = icyVector;
-icy.alt = 'coolest-cone';
+line2.innerHTML = 'chill with the <span class="cool">cool.</span>';
 
 headline.appendChild(line1);
 headline.appendChild(line2);
 home.appendChild(headline);
-home.appendChild(icy);
 content.appendChild(home);
 }
 
