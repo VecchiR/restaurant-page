1d4c01bcfea3d60c7521213fdcaf150e01d57be2
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-04-09 10:36:22 -0300

    Load basic store page content through module

--- src/store.js
+++ src/store.js
@@ -1,8 +1,70 @@
 export function loadStore() {
     const content = document.querySelector('#content');
+    
     const store = document.createElement('div');
     store.setAttribute('class', 'store-content');
-    store.innerHTML = 'This is the Store page!';
-    content.appendChild(store);
 
+    const hoursContainer = document.createElement('div');
+    hoursContainer.setAttribute('class', 'hours');
+    const contactContainer = document.createElement('div');
+    contactContainer.setAttribute('class', 'contact-info');
+    const locationContainer = document.createElement('div');
+    locationContainer.setAttribute('class', 'location');
+
+    const hoursSpan = document.createElement('span');
+    hoursSpan.setAttribute('class', 'title');
+    hoursSpan.innerHTML = 'Hours:';
+    const contactSpan = document.createElement('span');
+    contactSpan.setAttribute('class', 'title');
+    contactSpan.innerHTML = 'Contact Us:';
+    const locationSpan = document.createElement('span');
+    locationSpan.setAttribute('class', 'title');
+    locationSpan.innerHTML = 'Location:';
+
+    const hp1 = document.createElement('p');
+    const hp2 = document.createElement('p');
+    const hp3 = document.createElement('p');
+    const hp4 = document.createElement('p');
+    hp1.setAttribute('class', 'subtitle');
+    hp3.setAttribute('class', 'subtitle');
+    hp1.innerHTML = 'Wednesday - Sunday:';
+    hp2.innerHTML = '10:00 - 20:00';
+    hp3.innerHTML = 'Monday & Tuesday:';
+    hp4.innerHTML = 'Closed';
+
+    const cp1 = document.createElement('p');
+    const cp2 = document.createElement('p');
+    const cp3 = document.createElement('p');
+    const cp4 = document.createElement('p');
+    cp1.setAttribute('class', 'subtitle');
+    cp3.setAttribute('class', 'subtitle');
+    cp1.innerHTML = 'E-mail:';
+    cp2.innerHTML = 'icynicy@chillout.com';
+    cp3.innerHTML = 'Telephone:';
+    cp4.innerHTML = '(+99) 999-9999';
+
+    const lp1 = document.createElement('p');
+    lp1.innerHTML = 'Real St. 44, Right Here';
+
+    hoursContainer.appendChild(hoursSpan);
+    hoursContainer.appendChild(hp1);
+    hoursContainer.appendChild(hp2);
+    hoursContainer.appendChild(hp3);
+    hoursContainer.appendChild(hp4);
+
+    contactContainer.appendChild(contactSpan);
+    contactContainer.appendChild(cp1);
+    contactContainer.appendChild(cp2);
+    contactContainer.appendChild(cp3);
+    contactContainer.appendChild(cp4);
+
+    locationContainer.appendChild(locationSpan);
+    locationContainer.appendChild(lp1);
+
+    store.appendChild(hoursContainer);
+    store.appendChild(contactContainer);
+    store.appendChild(locationContainer);
+
+    content.appendChild(store);
 }
+
