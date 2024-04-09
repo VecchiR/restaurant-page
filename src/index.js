def372233dbf5af69b5baad178d9a8cf6bcfcca0
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-04-09 17:27:06 -0300

    Import image and change selected state of nav tabs

--- src/index.js
+++ src/index.js
@@ -1,30 +1,52 @@
 import { loadHome } from "./home";
 import { loadStore } from "./store";
 import { loadMenu } from "./menu";
+import './style.css';
+import icyVector from './resources/icy.svg';
+
 
 
 console.log("heyoo - I'm coming from src/index.js");
 loadHome();
+const icy = new Image();
+icy.src = icyVector;
+icy.alt = 'coolest-cone';
+document.querySelector('.image-container').appendChild(icy);
 
-
-const tabs = document.querySelectorAll('.tab-btn');
-tabs.forEach((btn) => btn.addEventListener('click', () => {
-    //aplicar um "check if tab = active" pra ver ser precisa carregar ou não ---------------------------------------------------
-    eraseContent();
-    if (btn.classList.contains('home')) {
-        loadHome();
-    }
-    else if (btn.classList.contains('menu')) {
-        loadMenu();
-    }
-    else if (btn.classList.contains('store')) {
-        loadStore();
-    }
-}));
+const homeTab = document.querySelector('.home');
+homeTab.addEventListener('click', selectTab);
+homeTab.classList.add('selected');
+const menuTab = document.querySelector('.menu');
+menuTab.addEventListener('click', selectTab);
+const storeTab = document.querySelector('.store');
+storeTab.addEventListener('click', selectTab);
 
 function eraseContent() {
     const content = document.querySelector('#content');
-    if(content.hasChildNodes()) {    
+    if (content.hasChildNodes()) {
         content.removeChild(content.firstChild);
+    }
+}
+
+
+function selectTab() { 
+    if (!this.classList.contains('selected')) {
+        console.log('iranbitch');
+        eraseContent();
+        homeTab.classList.remove('selected');
+        menuTab.classList.remove('selected');
+        storeTab.classList.remove('selected');
+        if (this.classList.contains('home')) {
+            loadHome();
+            homeTab.classList.add('selected');
+        }
+        else if (this.classList.contains('menu')) {
+            loadMenu();
+            menuTab.classList.add('selected');
+        }
+        else if (this.classList.contains('store')) {
+            loadStore();
+            storeTab.classList.add('selected');
+        }
     }
 }
